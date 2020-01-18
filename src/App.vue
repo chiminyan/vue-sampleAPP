13faeea2eb547b9a1e1c7a7317358437a149b090
Tomomi <sanctity3591@gmail.com>
2020-01-19 00:07:49 +0900

    add pageFooter

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <title-header></title-header>
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
 import titleHeader from './components/Header.vue'
+import pageFooter from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    'title-header': titleHeader
+    'title-header': titleHeader,
+    'page-footer': pageFooter
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page-footer">
+    <div class="icons">
+      <a href="https://twitter.com/chiminyan3591" target="blank"><img :src="twitter_src" alt="twitter"></a>
+      <a href="https:github.com/chiminyan/vue-sampleAPP" target="blank"><img :src="github_src" alt="github"></a>
+    </div>
+    <div class="copyright">
+      <p>©️   2020 ちみ</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pageFooter',
+  data () {
+    return {
+      twitter_src: require("../assets/footer/twitter.svg"),
+      github_src: require("../assets/footer/github.svg")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  background-color: rgb(105, 49, 119);
+  color: rgb(255, 226, 255);
+  height: 100px;
+}
+</style>
