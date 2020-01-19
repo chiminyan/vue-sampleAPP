c5cd30fc9bd256c7df2f70c1e324b401a0e60487
Tomomi <sanctity3591@gmail.com>
2020-01-19 15:01:46 +0900

    arrenge main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
 var app = new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-app.message = 'Hello!'
+app.config.productionTip = false
